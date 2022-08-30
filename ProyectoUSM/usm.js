fbc5d25821ca5b6884905561fc13878201f73bea
BMHV <94775980+Brakmel@users.noreply.github.com>
2022-08-30 17:08:18 -0400

    Add files via upload

---
+++ ProyectoUSM/usm.js
@@ -0,0 +1,24 @@
+function ocultar(){
+    document.getElementById('calendario_1').style.display = 'none';
+}
+function mostrar(){
+    document.getElementById('calendario_1').style.display = "block";
+}
+function ocultar1(){
+    document.getElementById('calendario_2').style.display = 'none';
+}
+function mostrar1(){
+    document.getElementById('calendario_2').style.display = "block";
+}
+function ocultar2(){
+    document.getElementById('calendario_3').style.display = 'none';
+}
+function mostrar2(){
+    document.getElementById('calendario_3').style.display = "block";
+}
+function ocultar3(){
+    document.getElementById('calendario_4').style.display = 'none';
+}
+function mostrar3(){
+    document.getElementById('calendario_4').style.display = "block";
+}
